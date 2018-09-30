f781823ce4cf1c278a5637b41fabc7e00b0bfc4c
German Hernandez <muygrafico@gmail.com>
2018-09-29 23:40:54 -0500

    fix App.js base component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-    import React from 'react'
-    import ReactDOM from 'react-dom'
-    const App = () => {
-        console.log(process.env.ENV_KEY)
-        return (
-            <div>
-                <p>Hello World</p>
-            </div>
-        )
-    }
-    export default App
-    ReactDOM.render(<App />, document.getElementById('app'))
+import React from 'react'
+
+export default () => {
+    console.log(process.env.ENV_KEY)
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+}
